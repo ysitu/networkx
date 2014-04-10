62b7b017d210a480a035da40ddd7511b6b45a2eb
ysitu <ysitu@users.noreply.github.com>
2014-04-10 09:01:43 -0400

    Add Edmonds-Karp maxflow algorithm

--- networkx/algorithms/flow/__init__.py
+++ networkx/algorithms/flow/__init__.py
@@ -1,9 +1,10 @@
-from . import maxflow, mincost, preflow_push, shortest_augmenting_path
+from . import maxflow, mincost, edmonds_karp, preflow_push, shortest_augmenting_path
 
-__all__ = sum([maxflow.__all__, mincost.__all__, preflow_push.__all__,
-               shortest_augmenting_path.__all__], [])
+__all__ = sum([maxflow.__all__, mincost.__all__, edmonds_karp.__all__,
+               preflow_push.__all__, shortest_augmenting_path.__all__], [])
 
 from .maxflow import *
 from .mincost import *
+from .edmonds_karp import *
 from .preflow_push import *
 from .shortest_augmenting_path import *
